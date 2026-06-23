13fd7eddeb09a153f7e3b077a51383820cc36277
kaiaiei <kai390322@gmail.com>
2026-06-22 18:00:49 -0700

    Update aes.sh

--- aes.sh
+++ aes.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Launch the first two immediately in the background (&)
+
 kitty --title "matrix_rain" -e unimatrix &
 kitty --title "desktop_clock" -e peaclock &
 
-# Stagger the next ones using backgrounded subshells
+
 (sleep 0.1 && kitty --title "audio_viz" -e cava) &
 (sleep 0.2 && kitty --title "sys_info" ) &
